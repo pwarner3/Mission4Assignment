b12bb4c987e65a75db32653f3e985c372c7428d4
Parker Warner <plouwarner@gmail.com>
2023-02-03 18:38:19 -0700

    more changes

--- Mission4Assignment/Controllers/Mission5Controller.cs
+++ Mission4Assignment/Controllers/Mission5Controller.cs
@@ -8,10 +8,13 @@ namespace Mission4Assignment.Controllers
 {
     public class Mission5Controller : Controller
     {
+        //Index page view
         public IActionResult Index()
         {
             return View();
         }
+
+        //Calculator page view
         public IActionResult Calculator()
         {
             return View();
